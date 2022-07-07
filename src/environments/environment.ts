12e4da3ab32fbcbd65a801699dc473bf9987918c
youssef <youssef.r.kamel.iti@gmail.com>
2022-07-07 10:45:44 +0200

    Update the environment URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  baseApi:"https://localhost:7043/api/"
+  baseApi:"https://yousefonlinestore.azurewebsites.net/"
 };
 
+// "https://localhost:7043/api/"
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
